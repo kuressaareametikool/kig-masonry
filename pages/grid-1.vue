1c237ecd4d7d7e23eb9946187633430a601da3b3
ooker <maaker+github@gmail.com>
2022-11-15 23:13:20 +0200

    grid-2

--- pages/grid-1.vue
+++ pages/grid-1.vue
@@ -7,7 +7,7 @@
         <ImageCard class="lg:row-start-4 lg:row-span-2 lg:col-start-2 lg:col-span-2">10</ImageCard>
 
         <div class="bg-slate-700 row-start-1 col-start-1 col-span-2 
-            lg:row-start-3 lg:col-start-2 lg:col-span-2
+            lg:row-start-3 lg:col-start-2 lg:col-span-2 
             flex justify-center items-center 
         ">
             <h1 class="text-slate-50 text-4xl md:text-[8vw] leading-none font-bold p-10">Minu portfoolio</h1>
---
+++ pages/grid-2.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="grid grid-cols-8 grid-rows-10 ">
+        
+        <ImageCard>1</ImageCard>
+        <ImageCard class="row-start-3 col-start-2">2</ImageCard>
+        <ImageCard class="row-start-1 col-start-6 col-span-3 row-span-3">3</ImageCard>
+        <ImageCard class="col-start-1 row-start-4 col-span-2 row-span-2">4</ImageCard>
+            
+        <div class="col-start-4 row-start-2 row-span-3 col-span-2 z-2
+        bg-slate-700 
+            flex justify-center items-center
+        ">
+            <p class="text-slate-50  lg:text-lg p-6">Grid'is võib olla vähem elemente kui gridi "ruute" ja neid saab siis loomin&shy;guliselt paigutada seal sees nagu malelaua nuppe.</p>
+        </div>
+    </div>
+
+</template>
