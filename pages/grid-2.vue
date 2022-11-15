90522208342f35750da02d49ec1cdd15d41d7016
ooker <maaker+github@gmail.com>
2022-11-15 23:39:54 +0200

    corneripõks

--- pages/grid-2.vue
+++ pages/grid-2.vue
@@ -8,10 +8,11 @@
         <ImageCard class="col-start-1 row-start-4 col-span-2 row-span-2">4</ImageCard>
             
         <div class="col-start-4 row-start-2 row-span-3 col-span-2 z-2
-        bg-slate-700 
+            bg-slate-700 
             flex justify-center items-center
+            rounded-br-[8vw] rounded-tl-[8vw]
         ">
-            <p class="text-slate-50  lg:text-lg p-6">Grid'is võib olla vähem elemente kui gridi "ruute" ja neid saab siis loomin&shy;guliselt paigutada seal sees nagu malelaua nuppe.</p>
+            <p class="text-slate-50 lg:text-lg p-6 ">Grid'is võib olla vähem elemente kui gridi "ruute" ja neid saab siis loomin&shy;guliselt paigutada seal sees nagu malelaua nuppe.</p>
         </div>
     </div>
 
